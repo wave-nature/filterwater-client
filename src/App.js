4c75da3c3463c34bcaa6d9f0f737330675794fa7
vivek30 <vs1010582@gmail.com>
2022-02-28 17:47:00 +0530

    super-admin dashboard on login fixed

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
           ) : (
             <Redirect to="/" />
           )}
-          {(userInfo && userInfo.role === "admin") ||
+          {(userInfo &&
+            (userInfo.role === "admin" || userInfo.role === "super-admin")) ||
           (user && (user.role === "admin" || user.role === "super-admin")) ? (
             <Route exact path="/admin" component={AdminDashboard} />
           ) : (
